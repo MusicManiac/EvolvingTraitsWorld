2b63a479436a15da067ade9705dfbf0d8bdb51c2
MusicManiac <14209235+MusicManiac@users.noreply.github.com>
2026-08-30 13:01:37 +0200

    fix: incorrect require

--- Contents/mods/Evolving Traits World/42.19/media/lua/server/ETW_ModDataServer.lua
+++ Contents/mods/Evolving Traits World/42.19/media/lua/server/ETW_ModDataServer.lua
@@ -6,7 +6,7 @@ local ETW_ModData = require("ETW_ModData")
 local ETW_EagleEyedTracking = require("TraitSpecific/ETW_EagleEyedTracking")
 local ETW_CommonLogicChecks = require("ETW_CommonLogicChecks")
 
-local ETW_BySkills = require("ETW_BySkills")
+local ETW_BySkills = require("DynamicLogic/ETW_BySkills")
 
 local Commands = {}
 
