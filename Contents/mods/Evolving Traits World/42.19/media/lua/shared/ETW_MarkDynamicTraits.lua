10e2006e2377aac220d230df496a512f12b945a6
Viaczeslav Kusznir <viaczeslav.kusznir@mga.com.pl>
2026-09-15 16:39:37 +0200

    feat: Mental State System

--- Contents/mods/Evolving Traits World/42.19/media/lua/shared/ETW_MarkDynamicTraits.lua
+++ Contents/mods/Evolving Traits World/42.19/media/lua/shared/ETW_MarkDynamicTraits.lua
@@ -20,6 +20,7 @@ local function registerDynamicTraits()
 	-- Custom ETW Traits
 	registerTraits({
 		ETW_Registry.traits.BLISSFUL,
+		ETW_Registry.traits.DEPRESSIVE,
 		ETW_Registry.traits.IDEAL_WEIGHT,
 		ETW_Registry.traits.AV_CLUB,
 		ETW_Registry.traits.AXE_THROWER,
